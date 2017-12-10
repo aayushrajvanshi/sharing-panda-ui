edf0a2b213760c3f3049dfab96ca5775ee280d26
Aayush Rajvanshi <aayush.rajvanshi@qantas.com.au>
2017-12-10 17:02:46 +0530

    Babel-polyfill added

--- src/client/constants/index.js
+++ src/client/constants/index.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = 'http://localhost:3000';
-// export const API_BASE_URL = 'https://sharing-panda-api.herokuapp.com';
+// export const API_BASE_URL = 'http://localhost:3000';
+export const API_BASE_URL = 'https://sharing-panda-api.herokuapp.com';
